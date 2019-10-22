8015c729f1f1f743bcb17006283be5979854823e
Andypad <56887463+Andypad@users.noreply.github.com>
2019-10-22 13:14:42 -0400

    Add files via upload

---
+++ procents/procents/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace procents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c,c1;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox3.Text);
+            c = a*100 / (100 + b);
+            c1 = Math.Round(c);
+            textBox2.Text = Convert.ToString(c1);
+        }
+    }
+}
